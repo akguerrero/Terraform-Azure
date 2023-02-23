f0ea376668f56a6bbbdae7f48ab9fe2393ab5d95
akguerrero <32792867+akguerrero@users.noreply.github.com>
2023-02-23 13:48:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id = "c9642077-afde-461c-9e7c-81536f8e0e97"
-    client_id       = "cb274029-8b54-4c2f-ad5b-f2d53d17506a"
-    client_secret   = "HwZ8Q~Ax73SPu~9DWkIRp.2OX3TecRQJM1LSraE7"
-    tenant_id       = "40e20b2c-2571-4106-a067-9cad17e7ef47"
 }
 resource "azurerm_resource_group" "rg" {
   name     = "terra-rg"
